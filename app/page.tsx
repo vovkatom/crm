79a822292a42672ee4e1536dd3c6569571ba32dc
Vladimir Bogachuk <vovkatom@gmail.com>
2024-06-01 21:42:43 +0300

    +3

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import ActiveLabel from "./components/active-label";
+import NotActiveLabel from "./components/not-active-label";
+
+
 export default function Home() {
   return (
     <main>
-      <h1>Privet Vovka1</h1>
+      <h1>Pruvit</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
   );
 }
